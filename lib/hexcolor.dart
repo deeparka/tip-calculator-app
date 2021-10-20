0b0d0548de7a9b682baa8a5719fecce4978d5a00
deeparka <arkadeep180@gmail.com>
2021-10-20 14:14:28 +0530

    added tip calculator app

---
+++ lib/hexcolor.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", " ");
+    if (hexColor.length == 0) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+/*
+Color color1 = HexColor("b74093");
+*/
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tip_calculator_app/test.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: BillSplitter(),
+  ));
+}
